b8616418f8988aecf8c08b9acb249d9ddaf8c1d6
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-08-20 00:17:28 +0700

    update new

--- src/common/cards/cardNImage/index.js
+++ src/common/cards/cardNImage/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+
+import Typography from '@material-ui/core/Typography'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
+import { Grid } from '@material-ui/core'
 
 const useStyles = makeStyles({
   root: {
@@ -14,32 +17,39 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
-});
+})
 
 export default function CardNImage() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant='h5' component='h2'>
             Lizard
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Lizards are a widespread group of squame reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+        <Grid
+          container
+          direction='row'
+          justifyContent='flex-end'
+          alignItems='center'
+          spacing={1}>
+          <Grid item>
+            <EditIcon />
+          </Grid>
+          <Grid item>
+            <DeleteIcon />
+          </Grid>
+        </Grid>
       </CardActions>
     </Card>
-  );
+  )
 }
