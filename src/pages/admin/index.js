5d0ce1fa83f78676501f136726aab2b47a3bece2
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-08-19 23:55:38 +0700

    commit branch dex

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -3,8 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import CardNImage from "../../common/cards/cardNImage";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,21 +27,10 @@ export default function AutoGrid() {
         <div className={classes.root}>
           <Grid container spacing={3}>
             <Grid item xs>
-              <Paper className={classes.paper}>xs</Paper>
+              <CardNImage />
             </Grid>
             <Grid item xs>
               <Paper className={classes.paper}>xs</Paper>
-            </Grid>
-            <Grid item xs>
-              <Paper className={classes.paper}>xs</Paper>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item xs>
-              <Paper className={classes.paper}>xs</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className={classes.paper}>xs=6</Paper>
             </Grid>
             <Grid item xs>
               <Paper className={classes.paper}>xs</Paper>
