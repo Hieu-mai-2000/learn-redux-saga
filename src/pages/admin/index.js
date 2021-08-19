3a41abe330fa37c51104469ef9fefb2784fb2656
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-08-20 00:18:48 +0700

    commit test new

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
-import CardNImage from "../../common/cards/cardNImage";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import CardNImage from '../../common/cards/cardNImage'
+import { Box } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,13 +12,13 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: "center",
+    textAlign: 'center',
     color: theme.palette.text.secondary,
   },
-}));
+}))
 
 export default function AutoGrid() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <React.Fragment>
@@ -27,17 +27,26 @@ export default function AutoGrid() {
         <div className={classes.root}>
           <Grid container spacing={3}>
             <Grid item xs>
-              <CardNImage />
+              <h1>TODO</h1>
+              <Box mt={1}>
+                <CardNImage />
+              </Box>
             </Grid>
             <Grid item xs>
-              <Paper className={classes.paper}>xs</Paper>
+              <h1>DOING</h1>
+              <Box mt={1}>
+                <CardNImage />
+              </Box>
             </Grid>
             <Grid item xs>
-              <Paper className={classes.paper}>xs</Paper>
+              <h1>DONE</h1>
+              <Box mt={1}>
+                <CardNImage />
+              </Box>
             </Grid>
           </Grid>
         </div>
       </Container>
     </React.Fragment>
-  );
+  )
 }
