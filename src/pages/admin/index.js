f82304c9190b75a504b06076377ec3fff644bdb7
Hieu-mai-2000 <hieu.mainguyenminh@hcmut.edu.vn>
2021-08-19 23:47:20 +0700

    commit test

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
@@ -18,29 +21,34 @@ export default function AutoGrid() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-      </Grid>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Paper className={classes.paper}>xs</Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className={classes.paper}>xs</Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className={classes.paper}>xs</Paper>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Paper className={classes.paper}>xs</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs>
+              <Paper className={classes.paper}>xs</Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </React.Fragment>
   );
 }
